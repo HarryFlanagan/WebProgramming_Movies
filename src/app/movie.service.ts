08fb21a848442b90c7a3d8987e3ff059a922b6a1
S00189591 <S00189591@VLAB-KCOMPY2-8>
2020-02-11 15:52:37 +0000

    addMovie() added with completed Form

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -7,13 +7,16 @@ export class MovieService {
 
   constructor() {}
   moviesList = [
-    {id: 1, title: 'The GodFather', year: '1972', director: 'Francis Ford Coppola'},
-    {id: 2, title: 'City of God', year: '2002', director: 'Fernando Meirelles'},
-    {id: 3, title: 'The Lord of the Rings: The Return of the King', year: '2003', director: 'Peter Jackson'},
+    {title: 'The GodFather', year: '1972', director: 'Francis Ford Coppola'},
+    {title: 'City of God', year: '2002', director: 'Fernando Meirelles'},
+    {title: 'The Lord of the Rings: The Return of the King', year: '2003', director: 'Peter Jackson'},
   ];
 
   getMovies() {
     return this.moviesList;
+  }
+  addMovie(movietitle:string, moviedirector:string, movieyear:string){
+    this.moviesList.push({title:movietitle, director:moviedirector, year:movieyear});
   }
   
 }
